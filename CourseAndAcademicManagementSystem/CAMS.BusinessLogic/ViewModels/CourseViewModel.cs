0080aa1da90dfc6a1145b28a17e3bbd80730415b
eslam_khalifa <khalifaeslam754@gmail.com>
2025-09-09 15:15:13 +0300

    chore: updated BLL structure by adding ViewModels folder and its files

---
+++ CourseAndAcademicManagementSystem/CAMS.BusinessLogic/ViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CAMS.BusinessLogic.Services.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMS.BusinessLogic.ViewModels
+{
+    public class CourseViewModel
+    {
+        
+    }
+}
