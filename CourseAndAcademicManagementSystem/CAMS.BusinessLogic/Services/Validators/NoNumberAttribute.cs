6b8baf87ae72350d97282f23f17c88b29b5dad31
eslam_khalifa <khalifaeslam754@gmail.com>
2025-09-09 14:57:25 +0300

    feat: implemented NoNumberAttribute custom validation class

--- CourseAndAcademicManagementSystem/CAMS.BusinessLogic/Services/Validators/NoNumberAttribute.cs
+++ CourseAndAcademicManagementSystem/CAMS.BusinessLogic/Services/Validators/NoNumberAttribute.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CAMS.BusinessLogic.Services.Validators
 {
-    internal class NoNumberAttribute
+    public class NoNumberAttribute : ValidationAttribute
     {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null) return ValidationResult.Success;
+
+            string str = value.ToString()!;
+            if (Regex.IsMatch(str, @"\d"))
+                return new ValidationResult("The field cannot contain numbers.");
+
+            return ValidationResult.Success;
+        }
     }
 }
