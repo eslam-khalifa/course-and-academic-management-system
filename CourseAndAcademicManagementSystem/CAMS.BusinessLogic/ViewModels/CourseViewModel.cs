5e0b59a605b4e605e73d8e63532970b93d680f2c
eslam_khalifa <khalifaeslam754@gmail.com>
2025-09-09 15:16:05 +0300

    feat: implemented CourseViewModel class

--- CourseAndAcademicManagementSystem/CAMS.BusinessLogic/ViewModels/CourseViewModel.cs
+++ CourseAndAcademicManagementSystem/CAMS.BusinessLogic/ViewModels/CourseViewModel.cs
@@ -10,6 +10,38 @@ namespace CAMS.BusinessLogic.ViewModels
 {
     public class CourseViewModel
     {
-        
+        public int CourseId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [NoNumber]
+        [Remote(action: "IsCourseNameUnique", controller: "Courses", AdditionalFields = "CourseId")]
+        public string Name { get; set; } = null!;
+
+        [StringLength(20)]
+        [Remote(action: "IsCourseCodeUnique", controller: "Courses", AdditionalFields = "CourseId")]
+        public string? Code { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; } = null!;
+
+        [StringLength(250)]
+        public string? ShortDescription { get; set; }
+
+        public string? Description { get; set; }
+
+        [Range(0, 10)]
+        public int? Credits { get; set; }
+
+        [Range(0, 1000)]
+        public int? DurationHours { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int? MaxTrainees { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public int? InstructorId { get; set; }
     }
 }
